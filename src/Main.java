1542cfb50338ca85178bfaf817a24a1588544c3d
Ikar-x <ikar86x@gmail.com>
2023-06-25 19:22:22 +0500

    lambda

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,18 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
-        ComparePerson cp = new ComparePerson();
+        Comparator<Person> cp = (o1, o2) -> {
+            String[] mas1 = o1.getSurname().split("\\P{IsAlphabetic}+");
+            String[] mas2 = o2.getSurname().split("\\P{IsAlphabetic}+");
+            if (mas1.length == mas2.length) return o1.getAge() - o2.getAge();
+            return mas1.length - mas2.length;
+        };
+
         List<Person> persons = new ArrayList<Person>();
 
         persons.add(new Person("Veniamin", "Nemirov Batareikin", 13));
